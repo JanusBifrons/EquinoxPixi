1ac2d16a142ee684206fb85719a999be165cf818
Alec Vickers <alecvickers@hotmail.com>
2024-02-06 20:01:42 +0000

    Tweaked scrap behaviour

--- src/game/objects/Scrap.ts
+++ src/game/objects/Scrap.ts
@@ -10,5 +10,15 @@ export class Scrap extends GameObject {
         Body.setPosition(body, body.position);
 
         this.setBody([body], true);
+
+        //this.container.position = body.position;
+
+        //this.draw(this.container);
+
+        this.body.frictionAir = 0.001;
+
+        const randomAngular = Math.random() > 0.5 ? 0.005 : -0.005;
+
+        Body.setAngularVelocity(this.body, this.body.angularSpeed + randomAngular);
     }
 }
